5bfd7185d96e47748898dd9692bedf821add1803
ehitco <2649426350@qq.com>
2020-12-22 02:31:14 +0800

    add: 常用数组操作

---
+++ javascript/Array/index.js
@@ -0,0 +1,12 @@
+// array.slice([fromIndex [, toIndex]]) 返回数组到一个片段
+const names = ['小智', '老王', '老李', '小三']
+const heroes = names.slice(0, 2)
+const tow = names.slice(2)
+console.log(heroes)
+console.log(tow)
+
+// array.includes() 返回一个布尔值
+
+// array.find(predicate) 方法返回数组中满足提供的测试函数的第一个元素的值。否则返回 undefined
+
+// 
