47543dd574b46a7779c65c4656f46ad0038fd74a
ehitco <aiden@ehnote.cn>
2021-09-07 11:54:55 +0800

    闭包高阶函数

---
+++ javascript/demo/chap04/sortBuy.js
@@ -0,0 +1,8 @@
+const sortBy = (property) => {
+  return (a,b) => {
+  var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+  return result;
+  }
+}
+
+sortBy('firstname')
