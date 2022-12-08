b2edc5cd7722e1e0ce8c35a00deab2686dd600b7
ehitco <aiden@ehnote.cn>
2022-12-08 16:17:10 +0800

    dist 目录推送到 gh-page分支

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ehitco/ehnote.git main:gh-pages
+git push -f git@github.com:ehitco/ehnote.git master:gh-pages
 
 cd -
